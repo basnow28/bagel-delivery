02a9bfd4b832034ecc2377e1170bbde8335528a0
Marianna <mana998@azet.sk>
2020-12-03 16:08:14 +0100

    Added dummy components

--- src/containers/Dashboard/index.js
+++ src/containers/Dashboard/index.js
@@ -4,6 +4,9 @@ import selectors from './selectors'
 import * as actions from './actions'
 import * as _ from 'lodash'
 import { RichText } from 'prismic-reactjs'
+import Navbar from '../Navbar'
+import DummyComponent from '../../components/DummyComponent'
+import './Dashboard.css';
 
 const Dashboard = props => {
     useEffect(() => {
@@ -12,8 +15,11 @@ const Dashboard = props => {
 
     console.log(props.content)
     return(
-        <div>
+        <div className="Dashboard">
             <h1>This is a dashboard!</h1>
+            <DummyComponent name = "What we do"></DummyComponent>
+            <DummyComponent name = "Who we work with"></DummyComponent>
+            <DummyComponent name = "Contact"></DummyComponent>
         </div>
     )
 }
