58875d12f96fb8411225d802cf0d360f69132fc0
Marianna <mana998@azet.sk>
2020-12-03 16:00:40 +0100

    Added Dummy Component for testing

---
+++ src/components/DummyComponent/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const DummyComponent = ({name}) => {
+    
+    return(
+        <section id={name}>
+        <h1 >{name}</h1>
+            <p><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/></p>
+        </section>
+    )
+
+}
+
+export default DummyComponent
