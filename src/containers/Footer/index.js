d6f187ca32e3463c3e1a9067dbe9ad6cc846689e
Dagmara <przygocka.dagmara@gmail.com>
2020-12-14 15:50:19 +0100

    fixed footer and text layout, added responsive behaviour
    to text from what we do section

--- src/containers/Footer/index.js
+++ src/containers/Footer/index.js
@@ -1,26 +1,27 @@
 import { extend } from 'lodash';
 import React from 'react';
+import './footer.css';
 
 class Footer extends React.Component{
     render(){
         return (
             <footer>
-                <div className='container'>
-                    <div className='row'>
-                        <div className='logoFooter col-lg-6 col-md-12'>
-                            <a href='#'>{this.props.logo}</a>
-                        </div>
-                        <div className='contactFooter col-lg-3 col-md-6'>
+                <div className='footer-container'>
+                    <div className='logoFooter'>
+                        <a href='#'>{this.props.logo}</a>
+                    </div>
+                    <div className='contact-social-footer'>
+                        <div className='contactFooter'>
                             <header>Contact</header>
-                            <a href='#' >{this.props.contact.number}</a><br/>
+                            <a href='#' >{this.props.contact.number}</a>
                             <a href='#' >{this.props.contact.email}</a>
                         </div>
-                        <div className='socialFooter col-lg-3 col-md-6'>
+                        <div className='socialFooter'>
                             <header>Social</header>
                             {this.props.social.map(s => 
-                                <a key = {s.name} href={s.link} >{s.name}<br/></a>
+                            <a key = {s.name} href={s.link} >{s.name}<br/></a>
                                 )
-                            }
+                            }   
                         </div>
                     </div>
                 </div>
